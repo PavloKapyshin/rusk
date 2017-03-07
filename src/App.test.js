9a2bce63c2b0db80eaf926cc603a4107d96f95da
Pavlo Kapyshin <i@93z.org>
2017-03-07 15:56:21 +0100

    Improve App test

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,6 @@ import App from "./App";
 
 
 it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
+    const container = document.createElement("div");
+    ReactDOM.render(<App />, container);
 });
